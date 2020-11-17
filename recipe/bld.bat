0d6da82f2cd275ca7cd830307be26eb7882a670e
David Brochart <david.brochart@gmail.com>
2020-11-17 02:27:30 +0100

    Build static lib too

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,26 @@
-mkdir "%SRC_DIR%"\build
-pushd "%SRC_DIR%"\build
+mkdir build-shared
+cd build-shared
 
 cmake -G "Ninja" ^
       -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
       -DCMAKE_INSTALL_LIBDIR=lib ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DBUILD_SHARED_LIBS=ON ^
+      ..
+if errorlevel 1 exit 1
+
+ninja install
+if errorlevel 1 exit 1
+
+cd ..
+mkdir build-static
+cd build-static
+
+cmake -G "Ninja" ^
+      -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+      -DCMAKE_INSTALL_LIBDIR=lib ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DBUILD_SHARED_LIBS=OFF ^
       ..
 if errorlevel 1 exit 1
 
